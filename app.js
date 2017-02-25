af8ca7813d8209e9bf6f21378e3b7173b2fc3439
laurent <laurent.saada@devinci.fr>
2017-02-25 10:52:53 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,110 +1,144 @@
 //importing modules
-var express = require( 'express' );
-var request = require( 'request' );
-var cheerio = require( 'cheerio' );
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
 
 //creating a new express server
 var app = express();
 
 //setting EJS as the templating engine
-app.set( 'view engine', 'ejs' );
+app.set('view engine', 'ejs');
 
 //setting the 'assets' directory as our static assets dir (css, js, img, etc...)
-app.use( '/assets', express.static( 'assets' ) );
+app.use('/assets', express.static('assets'));
 
-
-function callLeboncoin() {
-    var url = 'https://www.leboncoin.fr/ventes_immobilieres/1084457186.htm?ca=12_s'
-    var returnable=0;
-    request( url, function ( error, response, html ) {
-        if ( !error && response.statusCode == 200 ) {
-
-            var $ = cheerio.load( html )
-            var lbcDataArray = $( 'section.properties span.value' )
-
-            var lbcData = {
-                price: parseInt( $( lbcDataArray.get( 0 ) ).text().replace( /\s/g, '' ), 10 ),
-                city: $( lbcDataArray.get( 1 ) ).text().trim().toLowerCase().replace( /\_|\s/g, '-' ),
-                type: $( lbcDataArray.get( 2 ) ).text().trim().toLowerCase(),
-                surface: parseInt( $( lbcDataArray.get( 4 ) ).text().replace( /\s/g, '' ), 10 )
-    
-        }
-
-            console.log( lbcData )
-           // console.log( lbcData['price'])
-            returnable=lbcData['price']/lbcData['surface']
-            // console.log(returnable)
-        }
-        else {
-            console.log( error )
-        }
-        //console.log(returnable)
-        return returnable;
-    })
-
-}
 
 //makes the server respond to the '/' route and serving the 'home.ejs' template in the 'views' directory
-function callLemeilleuragent()
- {
-    var url = 'https://www.meilleursagents.com/prix-immobilier/poissy-78300/'
-    request( url, function ( error, response, html ) {
-        if ( !error && response.statusCode == 200 ) {
-
-            var $ = cheerio.load( html )
-           var MaDataArray = $( 'section.section div.prices-summary__cell--median')
-           //console.log(MaDataArray);
-            var MaData = {
-                price: parseInt( $( MaDataArray.get( 1) ).text().replace( /\s/g, '' ), 10 ),
+app.get('/', function (req, res) {
+    res.render('home');
+});
+app.get('/process', function (req, res) {
+    const url = req.query.lbcUrl;
+    if (url) {
+        getLBCData(url, res, getMAEstimation)
+    }
+    else {
+        res.render('home', { error: 'Url is empty' });
+    };
+});
+function getLBCData(lbcUrl, routeResponse, callback) {
+    request(lbcUrl, function (error, response, html) {               //fonction anonyme
+        if (!error) {
+            let $ = cheerio.load(html);
+            const lbcData = parseLBCData(html);
+            if (lbcData) {
+                console.log('LBC Data', lbcData)
+                callback(lbcData, routeResponse)
             }
-
-            console.log( MaData )
+            else {
+                routeResponse.render('home', { error: 'No data found' });
+            }
         }
         else {
-           console.log( error )
+            routeResponse.render('home', { error: 'Error loading the given URL' });
         }
-        console.log(MaData['price'])
-return MaData['price'];
-    })
-
-}
-
-function Compare()
-{
-var var1=callLeboncoin();
-var var2=callLemeilleuragent();
-var var3="";
-if(var1>var2)
-{
-var3=" le prix est trop élever par rapport au prix du marché";
-}
-else
-{
-var3="tu as fais une bonne affaire !!";
-}
-console.log(var3)
-}
-
-app.get( '/', function ( req, res ) {
-    var url = req.query.urlLBC
-    //callLeboncoin();
-    //callLemeilleuragent();
-    Compare();
-    
-
-    res.render( 'home', {
-        message: url
     });
-});
+}
+function parseLBCData(html) {
+    const $ = cheerio.load(html)
+    const lbcDataArray = $('section.properties span.value')
+    console.log(lbcDataArray)
+    return lbcData = {
+        price: parseInt($(lbcDataArray.get(0)).text().replace(/\s/g, ''), 10),
+        city: $(lbcDataArray.get(1)).text().trim().toLowerCase().replace(/\_|\s/g, '-').replace(/\-\d+/, ''),
+        postalCode: $(lbcDataArray.get(1)).text().trim().toLowerCase().replace(/\D|\-/g, ''),
+        type: $(lbcDataArray.get(2)).text().trim().toLowerCase(),
+        surface: parseInt($(lbcDataArray.get(4)).text().replace(/\s/g, ''), 10)
+    }
+}
+function isGoodDeal(lbcData, maData) {
 
-app.get( '/call', function ( req, res ) {
-    var url = req.query.urlLBC
-    res.render( 'home', { message: url });
+    const adPricePerSqM = Math.round(lbcData.price / lbcData.surface)
 
-});
-//getbootstrap.com/geting-started
+    const maPrice = lbcData.type === 'appartement' ? maData.priceAppart : maData.priceHouse
+
+    return adPricePerSqM < maPrice
+
+}
+function parseMAData(html) {
+
+    const priceAppartRegex = /\bappartement\b : (\d+) €/mi
+    const priceHouseRegex = /\bmaison\b : (\d+) €/mi
+
+    if (html) {
+        const priceAppart = priceAppartRegex.exec(html) && priceAppartRegex.exec(html).length === 2 ? priceAppartRegex.exec(html)[1] : 0
+        const priceHouse = priceHouseRegex.exec(html) && priceAppartRegex.exec(html).length === 2 ? priceHouseRegex.exec(html)[1] : 0
+
+        return {
+            priceAppart: parseInt(priceAppart, 10),
+            priceHouse: parseInt(priceHouse, 10)
+        }
+    }
+
+    return {}
+}
+function getMAEstimation(lbcData, routeResponse) {
+    if (lbcData.city && lbcData.postalCode && lbcData.surface && lbcData.price) {
+        const url = 'https://www.meilleursagents.com/prix-immobilier/{city}-{postalCode}/'.replace('{city}', lbcData.city.replace(/\_/g, '-')).replace('{postalCode}', lbcData.postalCode);
+        console.log('MON URL:', url)
+        request(url, function (error, response, html) {
+
+            if (!error) {
+
+                let $ = cheerio.load(html);
+
+                if ($('meta[name=description]').get().length === 1
+                    && $('meta[name=description]').get()[0].attribs
+                    && $('meta[name=description]').get()[0].attribs.content) {
+                    const maData = parseMAData($('meta[name=description]').get()[0].attribs.content);
+
+                    console.log('MA Data:', maData)
+
+                    if (maData.priceAppart && maData.priceHouse) {
+                        routeResponse.render('home', {
+                            data: {
+                                lbcData,
+                                maData,
+                                deal: {
+                                    good: isGoodDeal(lbcData, maData)
+                                }
+                            }
+                        })
+                    }
+                    else {
+                        routeResponse.render('home', {
+                            error: 'Error loading estimation data from MeilleursAgents'
+                        });
+                    }
+
+                }
+                else {
+                    routeResponse.render('home', {
+                        error: 'Error loading estimation data from MeilleursAgents'
+                    });
+                }
+            }
+            else {
+                routeResponse.render('home', {
+                    error: 'Error loading estimation data from MeilleursAgents'
+                });
+            }
+
+        })
+    }
+    else {
+        routeResponse.render('home', {
+            error: 'Fetched data is incorrect: ' + JSON.stringify(lbcData)
+        });
+    }
+}
 
 //launch the server on the 3000 port
-app.listen( 3000, function () {
-    console.log( 'App listening on port 3000!' );
+app.listen(3000, function () {
+    console.log('App listening on port 3000!');
 });
